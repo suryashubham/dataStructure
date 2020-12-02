52e1641aee1868b96c73fcbca4348de83e8988b7
suryashubham <35807369+suryashubham@users.noreply.github.com>
2020-12-02 16:45:15 +0530

    added logic to print top view of binary tree

--- vertical_order_operations_on_binary_tree.py
+++ vertical_order_operations_on_binary_tree.py
@@ -1,6 +1,9 @@
 vertical_maximum = 0
 vertical_minimum = 0
 
+level_pairs = dict()
+level_order_nodes = list()
+
 
 class NewNode:
     def __init__(self, data):
@@ -8,7 +11,7 @@ class NewNode:
         self.left = self.right = None
 
 
-arr = [1, 2, 3, 4, 5, 6, 7]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 q = []
 dia = 0
 
@@ -67,9 +70,29 @@ def sum_vertically():
         print(calculate_vertical_sum(root_node, 0, x, 0), end=' ')
 
 
+def top_view(root, index):
+    if root:
+        if len(level_order_nodes) > 0:
+            del level_order_nodes[0]
+        if index not in level_pairs.keys():
+            level_pairs[index] = root.data
+        level_order_nodes.append([root.left, index - 1])
+        level_order_nodes.append([root.right, index + 1])
+        top_view(level_order_nodes[0][0], level_order_nodes[0][1])
+
+
+def print_top_view():
+    for x in range(vertical_minimum, vertical_maximum + 1):
+        print(level_pairs[x], end=' ')
+
+
 if __name__ == "__main__":
     root_node = create_level_order_binary_tree(0)
     vertical_order(root_node, 0)
     travel_vertically()
     print()
     sum_vertically()
+    print()
+    top_view(root_node, 0)
+    print()
+    print_top_view()
