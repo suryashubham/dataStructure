61fdc337b0a1fdf391c71d241d0882762ca89741
suryashubham <35807369+suryashubham@users.noreply.github.com>
2020-12-13 21:42:00 +0530

    optimized the solution of leetcode bi-weekly challenge #41

--- sumOfAbsoluteDifference.js
+++ sumOfAbsoluteDifference.js
@@ -1,18 +1,17 @@
-//#TODO:optimization, as for large i/p Time Limit is getting exceeded
 var getSumAbsoluteDifferences = function(nums) {
     result = []
-    k= 0
-   for(i=0,j=0;i<nums.length&&j<nums.length;){
-    k = k + Math.abs(nums[i]-nums[j])
-    if(j<nums.length-1){
-        j++
+    biggerSum = 0
+    smallerSum = 0
+    for(i=0;i<nums.length;i++){
+        biggerSum=biggerSum+nums[i]
     }
-    else if(j == nums.length-1){
-        i++;
-        j = 0;
-        result.push(k)
-        k=0
+    for(i=0;i<nums.length;i++){
+        const num = nums[i]
+        biggerSum-=num
+        const smalldiff = (num*i)-smallerSum
+        const bigdiff = biggerSum-num*(nums.length-1-i)
+        result.push(smalldiff+bigdiff)
+        smallerSum += num
     }
-   }
-   return result
+    return result    
 };
