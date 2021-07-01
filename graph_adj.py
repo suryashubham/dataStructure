2fb1fa85e10ed207503a3e9d7303978b01c080f0
suryashubham <35807369+suryashubham@users.noreply.github.com>
2021-07-01 15:36:49 +0530

    graph implementation using adjacency matrix

---
+++ graph_adj.py
@@ -0,0 +1,44 @@
+# NODES = [0, 1, 2, 3]
+# EDGES = [(0, 2), (0, 1), (1, 2), (2, 0), (2, 3), (3, 3)]
+NODES = [1, 2, 3, 4, 5, 6, 7]
+EDGES = [(1, 2), (1, 3), (1, 4), (2, 1), (2, 4), (2, 5), (3, 1), (3, 4), (3, 7), (4, 2), (4, 3), (4, 6), (4, 1), (5, 2),
+         (5, 6), (5, 7), (6, 4), (6, 5), (6, 7), (7, 3), (7, 6), (7, 5)]
+
+from collections import deque
+
+
+class Graph:
+    def __init__(self, nv):
+        self.vertices = nv
+        self.adj_matrix = [[0] * nv for _ in range(nv)]
+
+    def add_edge(self, edge_set):
+        for src, des in edge_set:
+            if src < self.vertices and des < self.vertices:
+                self.adj_matrix[src][des] = 1
+                self.adj_matrix[des][src] = 1
+
+    def print_graph(self):
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                print(self.adj_matrix[i][j], end=' ')
+            print()
+
+    def BFS(self, start_node):
+        visited = [0] * self.vertices
+        q = deque([start_node])
+        visited[start_node] = 1
+        print('BFT')
+        while q:
+            item = q.popleft()
+            print(item, end=' ')
+            for i in range(self.vertices):
+                if self.adj_matrix[item][i] == 1 and visited[i] == 0:
+                    q.append(i)
+                    visited[i] = 1
+
+
+graph = Graph(len(NODES))
+graph.add_edge(EDGES)
+graph.print_graph()
+graph.BFS(1)
