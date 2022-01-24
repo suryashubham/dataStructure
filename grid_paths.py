fa5e8ccaa8d254ce17a546e1ddd2202578c79e49
Surya <shubhamsurya54@gmail.com>
2022-01-24 10:32:18 +0530

    deleted redundant files

--- grid_paths.py
+++
@@ -1,6 +0,0 @@
-def calculate_total_no_of_paths_in_grid(m, n):
-    if m == 1 or n == 1:
-        return 1
-    return calculate_total_no_of_paths_in_grid(m - 1, n) + calculate_total_no_of_paths_in_grid(m, n - 1)
-
-print(calculate_total_no_of_paths_in_grid(10, 10))
